8f04301095a8645bd7573c9eaf3263684837dd67
jenny <jenny@winlab.rutgers.edu>
2018-07-04 16:19:45 -0400

    added IMU to robot

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 parts={"rgb camera": "recieving visual data", "motors": "operating at optimal rpm", 
-        "battery": "fully charged", "GPS": "recieving location data", "depth camera": "recieving depth data"}
+        "battery": "fully charged", "GPS": "recieving location data", 
+        "IMU":"getting quaternions", "depth camera": "recieving depth data"}
 print("Hello, I am a robot")
 for part in parts:
     print("%s: %s" %(part, parts[part]))
