9e92557e1e13795e6d75a9b4c4f2cedc1c140b1d
jenny <jenny@winlab.rutgers.edu>
2018-07-04 15:28:26 -0400

    added depth camera to robot

--- robot.py
+++ robot.py
@@ -1,4 +1,5 @@
-parts={"camera": "recieving visual data", "motors": "operating at optimal rpm", "battery": "fully charged"}
+parts={"rgb camera": "recieving visual data", "motors": "operating at optimal rpm", 
+        "battery": "fully charged", "depth camera": "recieving depth data"}
 print("Hello, I am a robot")
 for part in parts:
     print("%s: %s" %(part, parts[part]))
