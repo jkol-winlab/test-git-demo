40b0f0c6323fe054a1eff356a9d8d1e7b95518b0
jenny <jenny@winlab.rutgers.edu>
2018-07-04 16:22:04 -0400

    Made robot more verbose

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 parts={"rgb camera": "recieving visual data", "motors": "operating at optimal rpm", 
         "battery": "fully charged", "depth camera": "recieving depth data"}
 print("Hello, I am a robot")
+print("These are the parts I have installed")
 for part in parts:
     print("%s: %s" %(part, parts[part]))
