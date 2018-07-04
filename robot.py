236c4361c1981465b3123057ca9d764b57ee02f1
jenny <jenny@winlab.rutgers.edu>
2018-07-04 15:47:17 -0400

    added accurate gps to robot

--- robot.py
+++ robot.py
@@ -1,5 +1,5 @@
 parts={"rgb camera": "recieving visual data", "motors": "operating at optimal rpm", 
-        "battery": "fully charged", "depth camera": "recieving depth data"}
+        "battery": "fully charged", "GPS": "recieving location data", "depth camera": "recieving depth data"}
 print("Hello, I am a robot")
 for part in parts:
     print("%s: %s" %(part, parts[part]))
