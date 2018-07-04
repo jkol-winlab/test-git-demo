10627c89b63a6352fe80e098b1bcd4dc78c02395
jenny <jenny@winlab.rutgers.edu>
2018-07-04 15:20:58 -0400

    added parts to robot

--- robot.py
+++ robot.py
@@ -1,1 +1,4 @@
+parts={"camera": "recieving visual data", "motors": "operating at optimal rpm", "battery": "fully charged"}
 print("Hello, I am a robot")
+for part in parts:
+    print("%s: %s" %(part, parts[part]))
