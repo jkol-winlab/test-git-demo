0aa8b09a395fc03d5ea627cf9ee4b7d350595bee
Jenny <jenny@winlab.rutgers.edu>
2018-07-05 10:28:29 -0400

    making data collection more verbose

--- robot.py
+++ robot.py
@@ -20,9 +20,13 @@ print("These are the parts I have installed")
 for part in parts:
     part.start()
 
+print("starting data collection")
+
 while True:
     for part in parts:
         part.getData()
+
+print("stopping data collection")
 
 for part in parts:
     part.stop()
