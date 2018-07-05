f4554a6e9e56ce69e579a8b738e5059f79ce7f92
jenny <jenny@winlab.rutgers.edu>
2018-07-04 20:47:54 -0400

    robot should have an adjective

--- robot.py
+++ robot.py
@@ -2,7 +2,7 @@ parts={"rgb camera": "recieving visual data", "motors": "operating at optimal rp
         "battery": "fully charged", "GPS": "recieving location data", 
         "IMU":"getting quaternions", "depth camera": "recieving depth data"
         "lidar":"getting range data"}
-print("Hello, I am a robot")
+print("Hello, I am a silly robot")
 print("These are the parts I have installed")
 for part in parts:
     print("%s: %s" %(part, parts[part]))
