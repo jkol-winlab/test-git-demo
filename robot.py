4eea4b0e520cb9247185113b77a512c9d1dd16c7
jenny <jenny@winlab.rutgers.edu>
2018-07-04 22:00:58 -0400

    handle vowels for adjective

--- robot.py
+++ robot.py
@@ -5,6 +5,15 @@ adjective=""
 if len(sys.argv)==2:
     adjective=sys.argv[1]
 
+if len(adjective)>0 and adjective[0] in ['a', 'e', 'i', 'o', 'u']:
+    adjective="an "+adjective
+elif len(adjective)>0:
+    adjective="a "+adjective
+else:
+    adjective="a"
+
+
+
 
 rgbcam=module("rgb camera", "recieving visual data", "rgb image")
 motors=module("motors", "operating at optimal rpm", "odometry")
@@ -15,7 +24,7 @@ depthcam=module("depth camera", "recieving depth data",  "depth image")
 
 parts=[rgbcam, motors, battery, gps, imu, depthcam]
 
-print("Hello, I am a %s robot"%adjective)
+print("Hello, I am %s robot"%adjective)
 print("These are the parts I have installed")
 for part in parts:
     part.start()
